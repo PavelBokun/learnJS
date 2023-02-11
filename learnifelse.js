f0a8e96500bff91a4e0d5de0e66372a269d4ae50
PavelBokun <pavel.snabteh@gmail.com>
2023-02-11 17:47:00 +0300

    ifelseadd

--- learnifelse.js
+++ learnifelse.js
@@ -17,7 +17,7 @@ function testElseIf(val) {
   testElseIf(7);
   testElseIf(10);
   testElseIf(12);
-  
+
   function orderMyLogic(val) {
     if (val < 5) {
       return "Less than 5";
@@ -31,3 +31,31 @@ function testElseIf(val) {
   orderMyLogic(4);
   orderMyLogic(6);
   orderMyLogic(11);
+  function testSize(num) {
+    // Only change code below this line
+  if (num<5){
+    return "Tiny";
+  } else if (num<10){
+  return "Small"
+  } else if (num<15){
+  return "Medium"
+  } else if (num<20){
+    return"Large"
+  } else if (num>=20){
+    return "Huge"
+  }
+    return "Change Me";
+  
+    // Only change code above this line
+  }
+  
+  testSize(0);
+  testSize(4);
+  testSize(5);
+  testSize(8);
+  testSize(10);
+  testSize(14);
+  testSize(15);
+  testSize(17);
+  testSize(20);
+  testSize(25);
