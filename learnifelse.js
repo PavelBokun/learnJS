1e27b9b7d067f6506ce17547047549454b30deb7
PavelBokun <pavel.snabteh@gmail.com>
2023-02-11 17:27:37 +0300

    ifelse

--- learnifelse.js
+++ learnifelse.js
@@ -17,3 +17,17 @@ function testElseIf(val) {
   testElseIf(7);
   testElseIf(10);
   testElseIf(12);
+  
+  function orderMyLogic(val) {
+    if (val < 5) {
+      return "Less than 5";
+    } else if (val < 10) {
+      return "Less than 10";
+    } else {
+      return "Greater than or equal to 10";
+    }
+  }
+  
+  orderMyLogic(4);
+  orderMyLogic(6);
+  orderMyLogic(11);
