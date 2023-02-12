0b0efb7e12d16d1ba5a64b02a8efd5d5e52fb10c
PavelBokun <pavel.snabteh@gmail.com>
2023-02-12 16:29:36 +0300

    ===

--- lesrnjsoper.js
+++ lesrnjsoper.js
@@ -8,12 +8,22 @@ function testEqual(val) {
   testEqual(10);
 //   ==
 function testStrict(val) {
-    if (val===7) { // Change this line
+  if (val===7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+console.log (testStrict(10));
+console.log (testStrict(7));
+console.log (testStrict("7"));
+// ===
+function testStrict(val,vali) {
+    if (val===vali) { // Change this line
       return "Equal";
     }
     return "Not Equal";
   }
   
-  console.log (testStrict(10));
-  console.log (testStrict(7));
+  console.log (testStrict(10,10));
   console.log (testStrict("7"));
