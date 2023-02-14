f9bcb25ea12b79da9b5d30132c5fee961b99ebe4
PavelBokun <pavel.snabteh@gmail.com>
2023-02-14 20:33:45 +0300

    kata

--- codewars.js
+++ codewars.js
@@ -1,6 +1,17 @@
+
+
 function isDivisible(n, x, y) {
     
     return (n % x === 0 && n % y === 0) ? true:false;
     
   }
   isDivisible(12,2,6)
+
+  function openOrSenior(data){
+    return data.map(([old, handicap]) => (old > 54 && handicap > 7) ? 'Senior' : 'Open'); 
+  }
+  function oc(d){
+    let r=(d>13.30 && h>8.00) ? "open":"close";
+    alert(r)
+  }
+  oc()
