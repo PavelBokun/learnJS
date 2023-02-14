83cf0a33616c373903070f712b5c3b31fbdfdf3c
PavelBokun <pavel.snabteh@gmail.com>
2023-02-14 21:55:58 +0300

    instanceof

--- learn.js
+++ learn.js
@@ -1,1 +1,6 @@
-let str="Hellow";
+// проверка конструктора
+function House(numBedrooms) {
+    this.numBedrooms = numBedrooms;
+  }
+  let myHouse=new House(4);
+  console.log(myHouse instanceof House);
