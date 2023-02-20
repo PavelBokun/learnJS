3b28b161869bcf9075ba41097697daa1415594b6
PavelBokun <pavel.snabteh@gmail.com>
2023-02-20 19:53:48 +0300

    funcmulti

--- learn.js
+++ learn.js
@@ -4,3 +4,20 @@ function House(numBedrooms) {
   }
   let myHouse=new House(7);
   console.log(myHouse instanceof House);
+  function prci(num){
+    let arr1=[];
+    let arr2=[];
+   let y= num.toString().split('').map(int => parseInt(int, 10));
+  // arr1=y;
+   arr2=y.reduce((sum, current) => sum * current)
+  // for (let i = 0; i < arr1.length; i++) {
+  //     const computation = arr1[i] * arr1[i];
+  //     arr2.push(computation);
+  //   }
+  // let y=(r%1===0);
+  // let y=(r%2===1)
+  alert(arr2)
+  }
+  
+  
+  prci(39)
